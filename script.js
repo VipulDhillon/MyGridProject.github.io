ddf49302ee4f39fd1df0cfef253f6db7ffb8d12a
VipulDhillon <vipuldhillon17@gmail.com>
2021-10-17 20:13:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-4+3;
+/* 4+3;
 
 if (4+3 === 7) {
-    alert("You are smart!");
+    alert("You are smart! as you have opened best website");
 }
+
+function sayhello() {
+    console.log("hello")
+}
+
+sayhello();
+
+var saybye = function() {
+    console.log("bye")
+}
+
+saybye();
+
+function multiply(a, b) {
+    return a*b;
+}
+multiply(3,4);*/
+
+var list = ["tiger", "cat", "bear", "bird"];
+console.log(list[0]);
