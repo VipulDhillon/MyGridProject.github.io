50dee8d0e9509379ecc221d48f6705748dd826a3
VipulDhillon <vipuldhillon17@gmail.com>
2021-10-27 21:27:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,5 +21,36 @@ function multiply(a, b) {
 }
 multiply(3,4);*/
 
-var list = ["tiger", "cat", "bear", "bird"];
-console.log(list[0]);
+/*var list = ["tiger", "cat", "bear", "bird"];
+console.log(list[0]);*/
+var database = [
+    {
+        username: "vipul",
+        password: "vipul1"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning!"
+    },
+    {
+        username: "sally",
+        timeline: "Javascript is sooooo cool!"
+    }
+];
+
+var userNamePrompt = prompt("what's your username");
+var passwordPrompt = prompt("what's yous password");
+
+function singIn(user, pass) {
+    if (user === database[0].username && 
+        pass === database[0].password) {
+            console.log(newsFeed);
+        } else {
+            alert("Sorry wrong username and password!")
+        }
+}
+
+singIn(userNamePrompt, passwordPrompt);
